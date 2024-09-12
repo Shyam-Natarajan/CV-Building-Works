c6b10262949256bc29968d2fa36442b1ac8c5ee6
N <shyam.n@dxc.com>
2024-09-12 18:26:11 +0530

    minor changes made

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 document.getElementById("contactForm").addEventListener("submit", function (event) {
     event.preventDefault()
+    var form = document.getElementById("contactForm");
     let name = document.getElementById("name").value.trim();
-
-    let message = document.getElementById("message").value;
 
     const phoneInput = document.getElementById('number').value.trim();
     const phonePattern = /^[0-9]{10}$/;
 
     if (phonePattern.test(phoneInput)) {
-        document.getElementById('formResponse').innerHTML = `<div class="alert alert-success">Phone number is valid!</div>`;
-        // You can proceed with form submission here
-        console.log(`Name = ${name}\nMobile Number = ${phoneInput}\nRequest = ${message}`);
+
+        fetch(form.action, {
+            method : "POST",
+            body: new FormData(document.getElementById("contactForm")),
+        }).then(
+            response => response.json()
+        )
 
         document.getElementById("formResponse").innerHTML = `<div class = " alert alert-success"> Thank you, ${name}! Your request has been sent successfully.</div>`;
     } else {
         document.getElementById('formResponse').innerHTML = `<div class="alert alert-danger">Invalid phone number. Please enter a 10-digit phone number.</div>`;
-        setTimeout(function() {
+        setTimeout(function () {
             location.reload(); // Reload the page after 3 seconds
         }, 3000);
     }
 
-    
+
 });
